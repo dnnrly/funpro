4307c2c4331bd422c11a2b271404e7167ca522ae
Pascal Dennerly <dnnrly@gmail.com>
2021-03-28 10:14:36 +0100

    Add initial integration test for ALB response

--- acceptance_test.go
+++ acceptance_test.go
@@ -2,18 +2,23 @@ package main_test
 
 import (
 	"fmt"
+	"net/http"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/cucumber/godog"
 	"github.com/stretchr/testify/assert"
 )
 
 type testContext struct {
-	err    error
-	result struct {
+	err       error
+	cmdResult struct {
 		Output string
 		Err    error
+	}
+	response struct {
+		code int
 	}
 }
 
@@ -26,19 +31,41 @@ func (c *testContext) Errorf(format string, args ...interface{}) {
 func (c *testContext) theAppRunsWithParameters(args string) error {
 	cmd := exec.Command("./funpro", strings.Split(args, " ")...)
 	output, err := cmd.CombinedOutput()
-	c.result.Output = string(output)
-	c.result.Err = err
+	c.cmdResult.Output = string(output)
+	c.cmdResult.Err = err
 
 	return nil
 }
 
 func (c *testContext) theAppExitsWithoutError() error {
-	assert.NoError(c, c.result.Err)
+	assert.NoError(c, c.cmdResult.Err)
+	return c.err
+}
+
+func (c *testContext) iMakeARequestTo(method, url string) error {
+	client := http.Client{
+		Timeout: time.Second * 5,
+	}
+	body := strings.NewReader("")
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	c.response.code = resp.StatusCode
+	return nil
+}
+
+func (c *testContext) theResponseCodeIs(expected int) error {
+	assert.Equal(c, expected, c.response.code)
 	return c.err
 }
 
 func (c *testContext) theOutputContains(expected string) error {
-	assert.Contains(c, c.result.Output, expected)
+	assert.Contains(c, c.cmdResult.Output, expected)
 	return c.err
 }
 
@@ -53,4 +80,6 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Step(`^the app runs with parameters "([^"]*)"$`, tc.theAppRunsWithParameters)
 	ctx.Step(`^the app exits without error$`, tc.theAppExitsWithoutError)
 	ctx.Step(`^the output contains "([^"]*)"$`, tc.theOutputContains)
+	ctx.Step(`^I make a "([^"]*)" to "([^"]*)"$`, tc.iMakeARequestTo)
+	ctx.Step(`^the response code is (\d+)$`, tc.theResponseCodeIs)
 }
---
+++ features/alb-requests.feature
@@ -0,0 +1,6 @@
+Feature: application help
+
+    @Integration
+    Scenario: Can get a 200 response using an ALB request
+        Given I make a "GET" to "http://funpro:8080/alb-responder/ok"
+        Then the response code is 200
